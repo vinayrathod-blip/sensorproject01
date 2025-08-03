6657aebe5141cec8c1f4efe100584d4a7ffd8031
Vinay Rathod <vinayrathod@example.com>
2025-08-03 17:01:13 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT ='-e.'
+def get_requirements(file_path:str)-> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+setup(
+    name = 'fault detection',
+    version= '0.01',
+    author = 'vinay rathod',
+    author_mail = '210010010064@iite.ac.in',
+    install_requirements= get_requirements('requirements.txt'),
+    packages=find_packages()
+
+)
